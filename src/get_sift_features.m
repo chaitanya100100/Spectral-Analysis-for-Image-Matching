66b7b512a7f2c26250eb90bb69accfe8dd87ae76
Chaitanya Patel <chaitanya100100@gmail.com>
2017-06-02 17:29:27 +0530

    eigen decomposition of laplacian

--- src/get_sift_features.m
+++ src/get_sift_features.m
@@ -1,4 +1,4 @@
-function [f, d] = get_sift_features(im_path)
+function [f, d, M, N] = get_sift_features(im_path)
 % Resize image so that total number of feature points doesn't exceed much.
 % There will be almost `max_pixels/(step*step)` feature points for small bin_size.
 % Extract image SIFT features using vlfeat.
@@ -20,6 +20,9 @@ im = single(im_raw_resized) ;
 % vl_dsift() does NOT compute a Gaussian scale space of the image I. 
 % Instead, the image should be pre-smoothed at the desired scale level, e.b. by using the vl_imsmooth() function. 
 
-[f, d] = vl_dsift(im, 'size', bin_size, 'step', step, 'verbose') ;
+% [f, d] = vl_dsift(im, 'size', bin_size, 'step', step, 'verbose') ;
+[f, d] = vl_dsift(im, 'size', bin_size, 'step', step) ;
+
+fprintf('total number of features : %d\n', size(d, 2)) ;
 
 end
--- src/main.m
+++ src/main.m
@@ -5,12 +5,11 @@ path2 = '2.jpg' ;
 
 % extract dense sift features from both images
 fprintf('dense sift features for %s\n', path1) ;
-[f1, d1] = get_sift_features(path1) ;
+[f1, d1, m1, n1] = get_sift_features(path1) ;
 fprintf('dense sift features for %s\n', path2) ;
-[f2, d2] = get_sift_features(path2) ;
+[f2, d2, m2, n2] = get_sift_features(path2) ;
 
-% nf1 will be number of vertices in graph from image1
-% nf2 will be number of vertices in graph from image2
+% number of feature points of image1 and image2 is nf1 and nf2 respectively
 nf1 = size(d1, 2) ;
 nf2 = size(d2, 2) ;
 nf = nf1 + nf2 ;
@@ -29,5 +28,13 @@ A = vertcat(A_upper, A_lower) ;
 
 % create laplacian matrix using formule L = D - A
 D = sum(A) ;
-A = -A ;
-A(1:nf+1:nf*nf) = D(:) ;
+L = -A ;
+L(1:nf+1:nf*nf) = D(:) ;
+
+[V, E] = eig(L) ;
+
+k1 = V(1:nf1, 2) ;
+k2 = V(nf1+1:nf, 2) ;
+
+[M1, M2] = meshgrid(1:n1, 1:m1) ;
+
